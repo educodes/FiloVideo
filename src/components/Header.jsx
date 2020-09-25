c522d63ce20be259e680626e58518d99a853e6b7
Eduardo Pérez <educodes0@gmail.com>
2020-09-25 13:27:47 -0500

    update [ images ]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../assets/styles/components/Header.scss'
-import logo from '../assets/static/logo-platzi-video-BW2.png';
+import logo from '../assets/static/logo_filovideo.png';
 import userIcon from '../assets/static/user-icon.png';
 
 const Header = () => (
@@ -8,7 +8,7 @@ const Header = () => (
     <img className="header__img" src={ logo } alt="FiloVideo" />
     <div className="header__menu">
       <div className="header__menu--profile">
-        <img src={ userIcon } alt="" />
+        <img src={ userIcon } alt="user_icon" />
         <p>Perfil</p>
       </div>
       <ul>

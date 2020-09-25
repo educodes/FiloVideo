a63229b1d71fe83c159e44d5921c2d53b647b518
Eduardo Pérez <educodes0@gmail.com>
2020-09-25 00:16:18 -0500

    add component and style [ Footer ]

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,12 +4,14 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 import '../assets/styles/App.scss'
 
 const App = () => (
   <div className="App">
     <Header/>
     <Search/>
+    
     <Categories>
      <Carousel>
        <CarouselItem/>
@@ -17,6 +19,8 @@ const App = () => (
        <CarouselItem/>
      </Carousel>
     </Categories>
+
+    <Footer/>
   </div>
 );
 

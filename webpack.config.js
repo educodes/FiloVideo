7673e4e37ab71564e9a9327ea1319bff07d04c8a
Eduardo Pérez <educodes0@gmail.com>
2020-09-25 00:53:35 -0500

    add and configure images [ webpack ]

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,18 @@ module.exports = {
           'css-loader',
           'sass-loader',
         ],
-      }, 
+      },
+      
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'assets/[hash].[ext]' },
+          }
+        ],
+      },
+      
     ],
   },
   plugins: [

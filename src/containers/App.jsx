bf21f3510dd6078e02cb07d4f0c27a60b56626b7
Eduardo Pérez <educodes0@gmail.com>
2020-09-25 19:28:27 -0500

    add [ hooks ] App

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,7 +7,21 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss'
 
-const App = () => (
+const API = 'http://localhost:3000/initalState'
+
+const App = () => {
+
+  const [ videos , setVideos] = useState([]); 
+
+  useEffect(() => {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => setVideos(data));
+  }, [])
+
+  console.log(videos);
+
+  return(
   <div className="App">
     <Header/>
     <Search/>
@@ -39,6 +53,6 @@ const App = () => (
 
     <Footer/>
   </div>
-);
+)};
 
 export default App

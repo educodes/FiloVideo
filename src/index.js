ad014deec388319a140006216ec96782ee9c950e
Eduardo Pérez <educodes0@gmail.com>
2020-09-29 15:31:11 -0500

    Rename App Component by Home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './containers/App.jsx';
+import Home from './containers/Home.jsx';
 
-ReactDOM.render(<App /> , document.getElementById('app'));
+ReactDOM.render(<Home /> , document.getElementById('app'));
 

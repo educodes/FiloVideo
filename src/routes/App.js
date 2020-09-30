4ed572ae03733165c69ecd0280deb8033c062af3
Eduardo Pérez <educodes0@gmail.com>
2020-09-29 19:45:34 -0500

    Add switch component

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../containers/Home.jsx';
 import Login from '../containers/Login.jsx';
 import Register from '../containers/Register.jsx';
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path = "/" component = { Home } />
-    <Route exact path = "/login" component = { Login } />
-    <Route exact path="/register" component={Register}/>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
   </BrowserRouter>
 );
 

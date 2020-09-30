828f546e85e12b0255574269b3970c5bba0d6fc2
Eduardo Pérez <educodes0@gmail.com>
2020-09-29 21:16:09 -0500

    Fragment NotFound component

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const NotFound = () =>(
-  <h1>Page not found 404</h1>
+  <>
+    <h1>Page not found 404</h1>
+    <h2>Regresar al Home</h2>
+  </>
 );
 
 export default NotFound;
